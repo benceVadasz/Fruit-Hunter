32dd05fcc19cef9a293a732ea18e1233c8d235e8
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-04-23 09:41:42 +0200

    initial commit

---
+++ page-templates/template-home.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Template: Home
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+
+
+
+get_footer();
